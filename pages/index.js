abc7e6c4ca2c3f3fc30e2a41c499c882b100ba14
noncivi <63532097+noncivi@users.noreply.github.com>
2025-06-05 14:06:17 +0900

    feat: update homepage

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>나만의 홈페이지</title>
+        <meta name="description" content="간단한 Next.js 홈페이지 예제" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="안녕하세요, 새 홈페이지에 오신 것을 환영합니다!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          이 페이지는 Next.js와 Netlify를 사용하여 만들어졌습니다.
         </p>
+        <ul>
+          <li>빠른 시작</li>
+          <li>쉬운 배포</li>
+          <li>React 기반</li>
+        </ul>
       </main>
 
       <Footer />
